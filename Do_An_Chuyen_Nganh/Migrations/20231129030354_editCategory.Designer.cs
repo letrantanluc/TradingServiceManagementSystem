cdd7f97c5c03310e68cd3d60cb02a920b36c4b37
Le Luc <Letrantanluc2002@gmail.com>
2023-11-30 17:52:32 +0700

    feat: edit ProductCategory

--- Do_An_Chuyen_Nganh/Migrations/20231129030354_editCategory.Designer.cs
+++ Do_An_Chuyen_Nganh/Migrations/20231129030354_editCategory.Designer.cs
@@ -4,6 +4,7 @@ using Do_An_Chuyen_Nganh.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Do_An_Chuyen_Nganh.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231129030354_editCategory")]
+    partial class editCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
