a174681b637648e80b7f29852844bfc8a5ef8bea
vuduchoainam <vinhoainam0815@gmail.com>
2023-12-04 12:15:54 +0700

    feat: Chat user to user (senderId, receiverId)

--- Do_An_Chuyen_Nganh/Hubs/ChatHub.cs
+++ Do_An_Chuyen_Nganh/Hubs/ChatHub.cs
@@ -1,13 +1,42 @@
-﻿using Do_An_Chuyen_Nganh.Models;
+﻿using Do_An_Chuyen_Nganh.Data;
+using Do_An_Chuyen_Nganh.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace Do_An_Chuyen_Nganh.Hubs
 {
     public class ChatHub :Hub
     {
-         public async Task SendMessage(string user, string message)
+        private readonly ApplicationDbContext _context;
+
+        public ChatHub(ApplicationDbContext context)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            _context = context;
+        }
+        public async Task SendMessage(string receiverId, string message)
+        {
+            var senderId = Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var chatMessage = new Message
+            {
+                Text = message,
+                SenderID = senderId,
+                ReceiverID = receiverId
+            };
+
+            _context.Messages.Add(chatMessage);
+            await _context.SaveChangesAsync();
+
+            await Clients.User(receiverId).SendAsync("ReceiveMessage", senderId, message);
+        }
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
         }
     }
 }
---
+++ Do_An_Chuyen_Nganh/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using Do_An_Chuyen_Nganh.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Do_An_Chuyen_Nganh.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public DateTime When { get; set; }
+
+        public string SenderID { get; set; }
+        public string ReceiverID { get; set; }
+        public Message()
+        {
+            When = DateTime.Now;
+        }
+    }
+}
