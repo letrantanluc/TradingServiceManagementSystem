259fb7e8494befc812af38e64d998222136faa48
vuduchoainam <vinhoainam0815@gmail.com>
2023-12-10 00:14:50 +0700

    feat: chat 99%

--- Do_An_Chuyen_Nganh/Hubs/ChatHub.cs
+++ Do_An_Chuyen_Nganh/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using Do_An_Chuyen_Nganh.Data;
 using Do_An_Chuyen_Nganh.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace Do_An_Chuyen_Nganh.Hubs
@@ -15,9 +16,10 @@ namespace Do_An_Chuyen_Nganh.Hubs
         }
         public async Task SendMessage(string receiverId, string message)
         {
+            Console.WriteLine($"SendMessage called: {Context.ConnectionId}");
+
             var senderId = Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var senderUsername = await GetUsernameFromUserId(senderId);
-
             var receiverUsername = await GetUsernameFromUserId(receiverId);
 
             var chatMessage = new Message
@@ -47,5 +49,26 @@ namespace Do_An_Chuyen_Nganh.Hubs
             var user = await _context.Users.FindAsync(userId);
             return user != null ? user.UserName : "Unknown User";
         }
+        public async Task<IEnumerable<Message>> GetMessages(string senderId, string receiverId)
+        {
+            var messages = await _context.Messages
+                .Where(m => (m.SenderID == senderId && m.ReceiverID == receiverId) ||
+                            (m.SenderID == receiverId && m.ReceiverID == senderId))
+                .OrderBy(m => m.When)
+                .ToListAsync();
+
+            return messages;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+
     }
 }
--- Do_An_Chuyen_Nganh/wwwroot/js/chat.js
+++ Do_An_Chuyen_Nganh/wwwroot/js/chat.js
@@ -2,17 +2,37 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-// Disable the send button until connection is established.
+// Tắt nút gửi cho đến khi kết nối được thiết lập.
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (username, message) {
+    console.log("ReceiveMessage called:", username, message);
+
     var li = document.createElement("li");
     li.textContent = `${username}: ${message}`;
     document.getElementById("messagesList").appendChild(li);
 });
 
+console.log("receiverId:", receiverId);
+
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
+
+    // Lấy tin nhắn khi kết nối được thiết lập
+    connection.invoke("GetMessages", senderId, receiverId)
+        .then(function (messages) {
+            console.log("Old messages received:", messages);
+
+            // Hiển thị tin nhắn trong chat-body
+            messages.forEach(function (message) {
+                var li = document.createElement("li");
+                li.textContent = `${message.senderID}: ${message.text}`;
+                document.getElementById("messagesList").appendChild(li);
+            });
+        })
+        .catch(function (err) {
+            console.error(err.toString());
+        });
 }).catch(function (err) {
     console.error(err.toString());
 });
@@ -31,3 +51,29 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     event.preventDefault();
     document.getElementById("messageInput").value = "";
 });
+
+function getReceiversBySender(senderId) {
+    // Call a hub method to get receivers for the specified sender
+    connection.invoke("GetReceiversBySender", senderId)
+        .then(function (receivers) {
+            console.log(`Receivers who messaged ${senderId}:`, receivers);
+
+            // Update the list-receiver with the received data
+            updateReceiverList(receivers);
+        })
+        .catch(function (err) {
+            console.error(err.toString());
+        });
+}
+
+function updateReceiverList(receivers) {
+    var listReceiver = document.querySelector('.list-receiver');
+    listReceiver.innerHTML = ''; // Clear the existing list
+
+    // Iterate through the receivers and update the list
+    for (var i = 0; i < receivers.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = receivers[i];
+        listReceiver.appendChild(listItem);
+    }
+}
